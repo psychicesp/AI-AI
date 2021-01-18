aada026de56781ce0e410976c44293e0063d547b
Eric S Penrose <ericspenrose@gmail.com>
2021-01-17 21:06:02 -0600

    Fresh Pickles

--- ML_Models/rf_model.py
+++ ML_Models/rf_model.py
@@ -12,22 +12,23 @@ with open('pickle_jar/split_data.pickle', 'rb') as dill:
     X_train, X_hp_train, X_test, y_train,y_hp_train, y_test = pickle.load(dill)
 
 #%%
-rf_model = RandomForestRegressor(random_state = 91, n_jobs = 1, max_depth = 30, max_features = 'sqrt', min_samples_leaf=1, min_samples_split=5, n_estimators = 250)
+rf_model = RandomForestRegressor(random_state = 91, n_jobs = -3, max_features = 'sqrt', min_samples_leaf=1, min_samples_split=4, n_estimators = 40, max_depth = 20)
 rf_model = rf_model.fit(X_train,y_train)
 
 print(f"Training Data Score: {rf_model.score(X_train, y_train)}")
 print(f"Testing Data Score: {rf_model.score(X_test, y_test)}")
 # %%
 param_grid = {
-    'min_samples_leaf': [1],
-    'min_samples_split': [4,5,6,7],
-    'n_estimators': [200,250,300],
+    'max_depth':[20,30,50,75],
+    'n_jobs':[1],
+    'min_samples_leaf': [1,2,3],
+    'min_samples_split': [3,4,5],
+    'n_estimators': [250,375,400,425],
     'max_features':['auto','sqrt'],
     'random_state':[91]
 }
 grid = GridSearchCV(rf_model, param_grid, verbose=3, cv = 5, n_jobs = -3)
 grid.fit(X_hp_train, y_hp_train)
-#%%
 print(grid.best_params_)
 print(grid.best_score_)
 #%%
