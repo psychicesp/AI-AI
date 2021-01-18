8141127f97be6d56d034d6cb59008d8aaef72db6
Eric S Penrose <ericspenrose@gmail.com>
2021-01-17 19:03:46 -0600

    Made Pickles

--- ML_Models/linear_model.py
+++ ML_Models/linear_model.py
@@ -16,10 +16,10 @@ X = df.drop('Price', axis = 1)
 y = df['Price']
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, train_size = 0.80)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=8, train_size = 0.80)
 
 #A second split for a Hyperparameter tuning set for a final Fit/Tune/Test split of 60/20/20
-X_train, X_hp_train, y_train, y_hp_train = train_test_split(X_train, y_train, random_state=0, train_size = 0.75)
+X_train, X_hp_train, y_train, y_hp_train = train_test_split(X_train, y_train, random_state=5, train_size = 0.75)
 split_data = (X_train, X_hp_train, X_test, y_train,y_hp_train, y_test)
 with open('pickle_jar/split_data.pickle', 'wb') as dill:
     pickle.dump(split_data,dill)
@@ -31,3 +31,5 @@ line_model = line_model.fit(X_train,y_train)
 print(f"Training Data Score: {line_model.score(X_train, y_train)}")
 print(f"Testing Data Score: {line_model.score(X_test, y_test)}")
 # %%
+with open('pickle_jar/line_model.pickle', 'wb') as bread_butter:
+    pickle.dump(line_model,bread_butter)
