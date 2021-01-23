3bcc9efdf5d733b98f3961e3470a88c2c1df9dd9
Regan <regan_maher@outlook.com>
2021-01-23 11:35:45 -0600

    Flask is rendering the website.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import pandas as pd
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, send_from_directory
 
 
 #################################################
@@ -32,9 +32,33 @@ app = Flask(__name__)
 #################################################
 @app.route("/")
 def home():
-    return render_template('static/index.html')
+    return render_template('index.html')
 
-@app.route("/Predictive_Analysis")
+@app.route("/index.html")
+def home2():
+    return render_template('index.html')
+
+@app.route('/Images/<path:path>')
+def send_images(path):
+    return send_from_directory('Images', path)
+
+@app.route('/Logos/<path:path>')
+def send_logo(path):
+    return send_from_directory('Logos', path)
+
+@app.route("/Market_Analytics.html")
+def Market_Analytics():
+    return render_template('Market_Analytics.html')
+
+@app.route("/StLouis_Overview.html")
+def StLouis_Overview():
+    return render_template('StLouis_Overview.html')
+
+@app.route("/About_Us.html")
+def About_Us():
+    return render_template('About_Us.html')
+
+@app.route("/Predictive_Analysis.html")
 def Predictive_Analysis():
     return render_template('Predictive_Analysis.html')
 
