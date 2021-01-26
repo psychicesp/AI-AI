d7f96f3487359ff2b3d9e0bcef4d43184a591dca
Eric S Penrose <ericspenrose@gmail.com>
2021-01-26 16:12:14 -0600

    Cluster Pickles

---
+++ ML_Models/Cluster/clust0.py
@@ -0,0 +1,122 @@
+#%%
+import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import make_scorer
+import numpy as np
+
+with open('cluster_pickles/df0.pickle','rb') as dill:
+    df = pickle.load(dill)
+
+X = df.drop('Price', axis = 1)
+y = df['Price']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=8, train_size = 0.80)
+X_train, X_hp_train, y_train, y_hp_train = train_test_split(X_train, y_train, random_state=5, train_size = 0.75)
+def scoring_function(y,x):
+    return np.sum(np.abs((x-y)/y) <0.05)/len(x)
+my_scorer = make_scorer(scoring_function, greater_is_better = True)
+#%%
+print('----------Linear Model----------')
+line0 = LinearRegression()
+line0 = line0.fit(X,y)
+
+print(f"Training Data Score: {line0.score(X, y)}")
+print(f"Custom Score: {scoring_function(y_test, line0.predict(X_test))}")
+print('--------------------------------')
+columns = X_train.columns
+weights = line0.coef_
+
+for i, column in enumerate(columns):
+    print(f"{column}: {weights[i]}")
+#%%
+knr0 = KNeighborsRegressor(
+    n_jobs = -3,
+    algorithm = 'auto',
+    leaf_size = 10,
+    n_neighbors = 20,
+    p = 1,
+    weights = 'distance')
+knr0 = knr0.fit(X_train,y_train)
+print('----------K Neighbors Regressor----------')
+print(f"Training Data Score: {knr0.score(X_train, y_train)}")
+print(f"Testing Data Score: {knr0.score(X_test, y_test)}")
+print(f"Custom Score: {scoring_function(y_test, knr0.predict(X_test))}")
+#%%
+param_grid = {
+    'n_neighbors':[3,4,5,6,7,8,9,10,15,20],
+    'leaf_size':[10,20,30,40,50,60,70,80,90,100],
+    'p': [1,2],
+    'weights':['uniform','distance'],
+    'algorithm':['auto', 'ball_tree', 'kd_tree'],
+    'n_jobs':[1]
+}
+grid = GridSearchCV(knr0, param_grid, verbose=4, cv = 5, n_jobs = -4, scoring = my_scorer)
+grid.fit(X_hp_train, y_hp_train)
+print(grid.best_params_)
+print(grid.best_score_)
+#%%
+rf0 = RandomForestRegressor(
+            random_state = 91, 
+            n_jobs = -3, 
+            max_features = 'sqrt', 
+            min_samples_leaf= 3,
+            min_samples_split=7, 
+            n_estimators = 66, 
+            max_depth = 12,
+            bootstrap = False)
+rf0 = rf0.fit(X_train,y_train)
+
+print('----------Random Forest Regressor----------')
+print(f"Training Data Score: {rf0.score(X_train, y_train)}")
+print(f"Testing Data Score: {rf0.score(X_test, y_test)}")
+print(f"Custom Score: {scoring_function(y_test, rf0.predict(X_test))}")
+# %%
+param_grid = {
+    'max_depth':[12,25,50,75,100,125,150],
+    'n_jobs':[1],
+    'min_samples_leaf': [2,3,4],
+    'min_samples_split': [6,7,8],
+    'n_estimators': [66,100,110,150,200,250],
+    'max_features':['auto','sqrt'],
+    'bootstrap':[True, False]
+}
+grid = GridSearchCV(rf0, param_grid, verbose=3, cv = 5, n_jobs = -3, scoring = my_scorer)
+grid.fit(X_hp_train, y_hp_train)
+print(grid.best_params_)
+print(grid.best_score_)
+#%%
+neural0 = MLPRegressor(
+    alpha = 0.00005,
+    learning_rate="adaptive",
+    learning_rate_init=0.01,
+    tol=0.000005
+)
+neural0 = neural0.fit(X_train,y_train)
+
+print('----------Neural Net----------')
+print(f"Training Data Score: {neural0.score(X_train, y_train)}")
+print(f"Testing Data Score: {neural0.score(X_test, y_test)}")
+print(f"Custom Score: {scoring_function(y_test, neural0.predict(X_test))}")
+# %%
+param_grid = {
+    'alpha':[0.00005, 0.00001, 0.000005, 0.000001],
+    'learning_rate': ['constant','adaptive'],
+    'learning_rate_init': [0.5, 0.1, 0.05, 0.01, 0.005],
+    'tol': [0.00005, 0.00001, 0.000005, 0.000001]
+}
+grid = GridSearchCV(neural0, param_grid, verbose=3, cv = 5, n_jobs = -3, scoring = my_scorer)
+grid.fit(X_hp_train, y_hp_train)
+print(grid.best_params_)
+print(grid.best_score_)
+
+# %%
+with open('cluster_pickles/model0.pickle', 'wb') as dill:
+    pickle.dump(rf0,dill)
+# %%
---
+++ ML_Models/neural_model.py
@@ -0,0 +1,49 @@
+#%%
+import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer
+import numpy as np
+#%%
+#A second split for a Hyperparameter tuning set for a final Fit/Tune/Test split of 60/20/20
+with open('pickle_jar/split_data.pickle', 'rb') as dill:
+    X_train, X_hp_train, X_test, y_train,y_hp_train, y_test = pickle.load(dill)
+
+def scoring_function(y,x):
+    return np.sum(np.abs((x-y)/y) <0.05)/len(x)
+my_scorer = make_scorer(scoring_function, greater_is_better = True)
+#%%
+rf_model = RandomForestRegressor(
+            random_state = 91, 
+            n_jobs = -3, 
+            max_features = 'sqrt', 
+            min_samples_leaf=2 ,
+            min_samples_split=8, 
+            n_estimators = 60, 
+            max_depth = 35,
+            bootstrap = False)
+rf_model = rf_model.fit(X_train,y_train)
+#%%
+print(f"Training Data Score: {rf_model.score(X_train, y_train)}")
+print(f"Testing Data Score: {rf_model.score(X_test, y_test)}")
+print(scoring_function(y_test, rf_model.predict(X_test)))
+# %%
+param_grid = {
+    'max_depth':[12,25,35],
+    'n_jobs':[1],
+    'min_samples_leaf': [2,3,4,5,6,7,8,9,10],
+    'min_samples_split': [4,5,6,7,8,9,10],
+    'n_estimators': [100,110,125,135],
+    'max_features':['auto','sqrt', 'log2'],
+    'bootstrap':[True, False]
+}
+grid = GridSearchCV(rf_model, param_grid, verbose=3, cv = 5, n_jobs = -3, scoring = my_scorer)
+grid.fit(X_hp_train, y_hp_train)
+print(grid.best_params_)
+print(grid.best_score_)
+#%%
+with open('pickle_jar/rf_model.pickle', 'wb') as bread_butter:
+    pickle.dump(rf_model,bread_butter)
+# %%
